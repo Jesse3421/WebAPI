99c91410d9da512c48418efce709036b8b0be64a
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2021-10-15 18:21:17 -0500

    updating array

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -8,71 +8,160 @@ const wrongTitle = document.querySelector("#wrong")
 const answerButtons = document.querySelector("#answer-buttons")
 let sec = 60
 var score = 0 
-let questionPool = [] //initialization condition
-let currentQuestionData, currentAnswerData 
+let currentQuestion, currentAnswerData, randomQuestion 
 
 
 var questionArr = [
 
     {
-        questions: 'Which actor originally played James Rhodes in the original Iron Man film?',
-        answers: [
-            {text: 'Paul Bethany', answer: false},
-            {text: 'Terrance Howard', answer: true},
-            {text: 'Don Cheadle', answer: false},
-            {text: 'Jon Favreau', answer: false}
-    ]},
+        question: 'Which actor originally played James Rhodes in the original Iron Man film?',
+        choices: [
+             'Paul Bethany', 
+             'Terrance Howard', 
+             'Don Cheadle', 
+             'Jon Favreau'
+        ],
+        answer: 'Don Cheadle' 
+    },
     {
-        questions: 'In which movie did we first see the face of Thanos?',
-        answers: [
-            {text: 'Thor', answer: false},
-            {text: 'The Avengers', answer: true},
-            {text: 'The Avengers: Age of Ultron', answer: false},
-            {text: 'The Avenger: Endgame', answer: false}
-        ]},
-       {
-        questions: 'Who played Thor’s love interest in the first two Thor films?',
-        answers: [
-            {text: 'Scarlet Johansson', answer: false},
-            {text: 'Elizabeth Olson', answer: false},
-            {text: 'Brie Larson', answer: false},
-            {text: 'Natalie Portman', answer: true}
-        ]},
-        {
-        questions: 'Natasha Romanoff tells Loki she has what in her ledger?',
-        answers: [
-            {text: 'Phone Numbers', asnswer: false},
-            {text: 'Red', answer: true},
-            {text: 'Plans', answer: false},
-            {text: 'Pictures of friends', answer: false}
-        ]},
-        {
-        questions: 'Bradley Cooper is the voice of which Marvel character?',
-        answers: [
-            {text: 'Groot', answer: false},
-            {text: 'Howard the Duck', answer: false},
-            {text: 'Rocket', answer: true},
-            {text: 'Jarvis', answer: false}
-        ]}
-        
+        question: 'In which movie did we first see the face of Thanos?',
+        choices: [
+            'Thor',
+            'The Avengers',
+            'The Avengers: Age of Ultron',
+            'The Avenger: Endgame'
+        ],
+        answer: 'The Avengers: Age of Ultron',
+    },
+    {
+        question: 'Who played Thor’s love interest in the first two Thor films?',
+        choices: [
+            'Scarlet Johansson', 
+            'Elizabeth Olson', 
+            'Brie Larson', 
+            'Natalie Portman',
+        ],
+        answer: 'Natalie Portman',
+    },
+    {
+        question: 'Natasha Romanoff tells Loki she has what in her ledger?',
+        choices: [
+            'Phone Numbers', 
+            'Red', 
+            'Plans', 
+            'Pictures of friends', 
+        ],
+        answer: 'Red',
+    },
+    {
+        question: 'Bradley Cooper is the voice of which Marvel character?',
+        choices: [
+            'Groot', 
+            'Howard the Duck', 
+            'Rocket', 
+            'Jarvis', 
+        ],
+        answer: 'Rocket'
+    }
     
 ]
 //Press Start button to begin game
 
 startButton.addEventListener("click" , startQuiz)
-answerEl.addEventListener("click", presentNextQuestion)
+//answerEl.addEventListener("click", validateAnswer)
 
 function startQuiz() {
     introContainer.style.display = "none"
     questionContainer.style.display = "flex"
     score = 0
     currentQuestion = 0
+    question = currentQuestion.question
+    randomQuestion = Math.floor(Math.random() * questionArr.length)
+    displayNextQuestion()    
+}
+
+    //Display next random question in index
+function displayNextQuestion() {
+        displayQuestion(randomQuestion[currentQuestion])
+}
     
-    presentNextQuestion()
-    //TIMER for quiz  
-    //var time = setInterval(myTimer, 1000);
+    //Display current question in the button
+function displayQuestion(currentQuestion) {
+        questionEl.innerText = .question
+        console.log(currentQuestion.question)
+}
+
+  /*  function validateAnswer(currentQuestion) {
+        currentQuestion.choices.forEach(function(choice, index){
+        
+        })
+    }*/
+
+    function presentNextQuestion(questionArr) {
+        currentQuestion = currentQuestion.title
+        console.log(currentQuestion)
+        
+            console.log(currentAnswer)
+            currentQuestionData = questionArr[questionIndex]
+            currentAnswerData = currentQuestionData.answers[i].text
+            
+            currentAnswer = questionPool[i].answers[i].
+            currentAnswer = currentQuestion.answers[i]
+            var btn = document.querySelector(`#btn${i+1}`)
+            //currentAswers = currentQuestion.answer.text
+            btn.innerText = currentAnswer 
+        
+        
+        
+        
+        for (let i = 0; i < questionArr.length; i++) { 
+            currentQuestion = questionArr[i]
+            console.log(currentQuestion)
+            
+            //currentAnswer = questionPool[i].answers[i].
+            currentAnswer = currentQuestion.answers[i]
+            console.log(currentAnswer)
+            questionIndex = Math.floor(Math.random() * questionPool.length)
+            currentQuestionData = questionPool[questionIndex]
+            currentAnswerData = currentQuestionData.answers[i].text
+            questionEl.innerText = currentQuestion[i].questions
+            
+            var btn = document.querySelector(`#btn${i+1}`)
+            //currentAswers = currentQuestion.answer.text
+            btn.innerText = currentAnswer
+            
+            
+            if (answerChoice) {
+                //questionPool.splice(currentQuestionData) 
+                score = score + 25
+                correctTitle.classList.remove("hidden")
+                setTimeout(clearStatusClass, [2000]) 
+                presentNextQuestion
+            } else {
+                //questionPool.splice(currentQuestionData)
+                score = score - 25
+                sec = sec - 10
+                wrongTitle.classList.remove("hidden")
+                setTimeout(clearStatusClass, [2000]) 
+                presentNextQuestion
+            }
+        }
+    }
     
-    myTimer = () => {
+    
+    
+    function clearStatusClass() {
+        correctTitle.classList.add("hidden")
+        wrongTitle.classList.add("hidden")
+        answerEl.classList.add("btn-class")
+        presentNextQuestion()
+        
+    }
+    
+    
+        
+     /*  //TIMER for quiz  
+    function myTimer() {
         document.querySelector('#timer').innerHTML = "Time: " + sec;
         sec--;
         if (sec == -1) {
@@ -81,112 +170,6 @@ function startQuiz() {
             //questionContainer.style.display = "none"
             //insert last slide here closingContainer.style.remove ="hide"
             //Display Score....
-        }
-    }
-}
-
-console.log(questionArr)
-
-function presentNextQuestion(questionPool) {
-    
-    questionPool = questionArr.slice()
-    console.log(questionPool)
-    
-    
-    for (let i = 0; i < questionPool.length; i++) { 
-       currentQuestion = questionPool[i]
-       console.log(currentQuestion)
-       currentAnswer = questionPool[i].answers[i].
-       console.log(currentAnswer)
-        questionIndex = Math.floor(Math.random() * questionPool.length)
-        currentQuestionData = questionPool[questionIndex]
-        currentAnswerData = currentQuestionData.answers[i].text
-        questionEl.innerText = currentQuestion[i].questions
-
-        var btn = document.querySelector(`#btn${i+1}`)
-        //currentAswers = currentQuestion.answer.text
-        btn.innerText = currentAnswerData
- 
-
-            if (answerChoice) {
-                questionPool.splice(currentQuestionData) 
-                score = score + 25
-                correctTitle.classList.remove("hidden")
-                setTimeout(clearStatusClass, [2000]) 
-                presentNextQuestion
-                
-            } else {
-                questionPool.splice(currentQuestionData)
-                score = score - 25
-                sec = sec - 10
-                wrongTitle.classList.remove("hidden")
-                setTimeout(clearStatusClass, [2000]) 
-                presentNextQuestion
-            }
-        }
-    }  
-      
-        function clearStatusClass() {
-            correctTitle.classList.add("hidden")
-            wrongTitle.classList.add("hidden")
-            answerEl.classList.add("btn-class")
-            presentNextQuestion()
-                
-        }
-        
- 
-        
-        /*
-        function hide(evt){
-            evt.target.answerEl.style.display = "none"
-            answerEl.classList
-            questionPool = questionArr.slice()
-            
-            
-            questionPool.splice(randomQuestion)
-
-    if(questionPool[0].answers[i].answer) {
-        answerEl.dataset.correct = answers.correct
-    }
-    answerEl.addEventListener("click", chooseAnswer)
-    
-  currentQuestion = randomQuestion.questions
-}
-
-chooseAnswer(e)
-    var selectedAnswer = e.target
-
-       
-    
-
-    
-    //answerButtons.appendChild("answerEl")
-    
-   
-    if (questionPool > 0)
-    presentNextQuestion()
-    } else {
-        endQuiz ()
-    }
-
-}      
-
-
-function chooseAnswer(e) {
-
-answerEl.addEventListener('click', correct)
-
-}function repeat() {
-    if(questionPool >= 0)
- presentNextQuestion()
- } else {
-    endGame()
- }
- 
-
-function clearStatusClass(element) {
-    correctAnswer.classList.add('hidden')
-    incorrectAnswer.classList.add('hidden')
- }
-  
- */
+        }   
+        var time = setInterval(myTimer, 1000);
+    }   */
