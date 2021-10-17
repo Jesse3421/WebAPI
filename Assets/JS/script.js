1cc3264543eee12799d2b81234645c2b45f4e31b
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2021-10-17 11:23:12 -0500

    working on timer

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -6,9 +6,13 @@ const answerEl = document.querySelector(".btn-class")
 const correctTitle = document.querySelector("#correct")
 const wrongTitle = document.querySelector("#wrong")
 const answerButtons = document.querySelector("#answer-buttons")
+const endGamePage = document.querySelector("#endGame-slide")
+const scoreShow = document.querySelector(".scorePlaceholder")
+const timeShow = document.querySelector("#timer")
+
 let sec = 60
-var score = 0 
-let currentQuestion, currentAnswer, randomQuestion 
+var score = 0
+let currentQuestion, currentAnswer, answer, timerId
 
 
 var questionArr = [
@@ -16,12 +20,12 @@ var questionArr = [
     {
         question: 'Which actor originally played James Rhodes in the original Iron Man film?',
         choices: [
-             'Paul Bethany', 
-             'Terrance Howard', 
-             'Don Cheadle', 
-             'Jon Favreau'
+            'Paul Bethany',
+            'Terrance Howard',
+            'Don Cheadle',
+            'Jon Favreau'
         ],
-        answer: 'Don Cheadle' 
+        answer: 'Terrance Howard'
     },
     {
         question: 'In which movie did we first see the face of Thanos?',
@@ -36,9 +40,9 @@ var questionArr = [
     {
         question: 'Who played Thor’s love interest in the first two Thor films?',
         choices: [
-            'Scarlet Johansson', 
-            'Elizabeth Olson', 
-            'Brie Larson', 
+            'Scarlet Johansson',
+            'Elizabeth Olson',
+            'Brie Larson',
             'Natalie Portman',
         ],
         answer: 'Natalie Portman',
@@ -46,132 +50,116 @@ var questionArr = [
     {
         question: 'Natasha Romanoff tells Loki she has what in her ledger?',
         choices: [
-            'Phone Numbers', 
-            'Red', 
-            'Plans', 
-            'Pictures of friends', 
+            'Phone Numbers',
+            'Red',
+            'Plans',
+            'Pictures of friends',
         ],
         answer: 'Red',
     },
     {
         question: 'Bradley Cooper is the voice of which Marvel character?',
         choices: [
-            'Groot', 
-            'Howard the Duck', 
-            'Rocket', 
-            'Jarvis', 
+            'Groot',
+            'Howard the Duck',
+            'Rocket',
+            'Jarvis',
         ],
-        answer: 'Rocket'
+        answer: 'Rocket',
     }
     
+
 ]
 //Press Start button to begin game
 
-startButton.addEventListener("click" , startQuiz)
-//answerEl.addEventListener("click", validateAnswer)
+startButton.addEventListener("click", startQuiz)
+sec = 60
+score = 0
+currentQuestion = 0
+currentAnswer = 0
+answerChoices = questionArr[currentQuestion].choices
+question = questionArr[currentQuestion].question
+answer = questionArr[currentQuestion].answer
 
 function startQuiz() {
+    myTimer()
     introContainer.style.display = "none"
     questionContainer.style.display = "flex"
-    score = 0
-    currentQuestion = 0
-    currentAnswer = 0
-    answerChoices = questionArr[currentQuestion].choices
-    console.log(answerChoices)
-    question = questionArr[currentQuestion].question
-    console.log(question)
-    //randomQuestion = Math.floor(Math.random() * questionArr.length)
-    displayQuestion(question, answerChoices) 
-    console.log(question)   
+    displayQuestion(question, answerChoices, answer)
+    timeId = setInterval(myTimer, 1000);
 }
+
+
+
 //Display current question in the button
-function displayQuestion (question, answerChoices) {
+function displayQuestion(question, answerChoices, answer) {
+    console.log('answerChoices: ', answerChoices);
     questionEl.innerText = question
-    console.log(question)
-    var btn = document.querySelector(`#btn${i+1}`)
-    console.log(btn)
-    console.log(answerChoices)
-    btn.innerText = answerChoices
+    for (i = 0; i < answerChoices.length; i++) {
+        var btn = document.querySelector(`#btn${i + 1}`)
+        btn.innerText = answerChoices[i]
+        btn.dataset.text = answerChoices[i]
+        btn.addEventListener("click", validateAnswer)
+    }
+    if(sec === 0) () => endGame
+}
+
+function validateAnswer() {
+    
+    let selectedAnswer = this.dataset.text
+    console.log('selectedAnswer: ', selectedAnswer);
+    console.log(selectedAnswer === answer)
+    if (selectedAnswer === answer) {
+        console.log('sec: ', sec);
+        score = score + 25
+        console.log(score);
+        correctTitle.classList.remove("hidden")
+        setTimeout(clearStatusClass, [2000])
+        sec = sec + 10
+        return
+    }
+    score = score - 25
+    sec = sec - 10
+    console.log('sec: ', sec);
+    wrongTitle.classList.remove("hidden")
+    setTimeout(clearStatusClass, [1000])
+    return
+}
+
+
+
+function clearStatusClass() {
+    currentQuestion++
+    correctTitle.classList.add("hidden")
+    wrongTitle.classList.add("hidden")
+    answerEl.classList.add("btn-class")
+    console.log(currentQuestion)
+    if (currentQuestion > 4 || sec === 0) {
+        console.log(sec)
+        console.log(currentQuestion.length);
+        clearInterval(timeId)
+        endGame()
+    } else {
+        question = questionArr[currentQuestion].question
+        answerChoices = questionArr[currentQuestion].choices
+        answer = questionArr[currentQuestion].answer
+        displayQuestion(question, answerChoices, answer)
+    }
+}
+
+function endGame() {
+    clearInterval(timeId)
+    questionContainer.style.display = "none"
+    endGamePage.style.display = "flex"
+    document.querySelector()
+    scoreShow.innerHTML = "Score: " + score
+    storage.setItem(playerScore, score)
     
 }
 
-    function validateAnswer(currentQuestion) {
-        currentQuestion.choices.forEach(function(choice, index){
-        if(userChoice === questions[currentQuestion].answer){
+function myTimer() {
+    sec--;
+    timeShow.innerHTML = "Time: " + sec
+    
+}
 
-        }
-        })
-    }
-
-    function presentNextQuestion(questionArr) {
-        currentQuestion = currentQuestion.question
-        console.log(currentQuestion)
-        
-        
-            currentQuestionData = questionArr[questionIndex]
-            currentAnswerData = currentQuestionData.answers[i].text
-            
-            currentAnswer = questionPool[i].answers[i].
-            currentAnswer = currentQuestion.answers[i]
-        
-        
-        
-        
-        /*for (let i = 0; i < questionArr.length; i++) { 
-            currentQuestion = questionArr[i]
-            console.log(currentQuestion)
-            
-            //currentAnswer = questionPool[i].answers[i].
-            currentAnswer = currentQuestion.answers[i]
-            console.log(currentAnswer)
-            questionIndex = Math.floor(Math.random() * questionPool.length)
-            currentQuestionData = questionPool[questionIndex]
-            questionEl.innerText = currentQuestion[i].questions
-            
-            var btn = document.querySelector(`#btn${i+1}`)
-            //currentAswers = currentQuestion.answer.text
-            btn.innerText = currentAnswer
-            
-            
-            if (answerChoice) {
-                //questionPool.splice(currentQuestionData) 
-                score = score + 25
-                correctTitle.classList.remove("hidden")
-                setTimeout(clearStatusClass, [2000]) 
-                presentNextQuestion
-            } else {
-                //questionPool.splice(currentQuestionData)
-                score = score - 25
-                sec = sec - 10
-                wrongTitle.classList.remove("hidden")
-                setTimeout(clearStatusClass, [2000]) 
-                presentNextQuestion
-            }
-        }*/
-    }
-    
-    
-    
-    function clearStatusClass() {
-        correctTitle.classList.add("hidden")
-        wrongTitle.classList.add("hidden")
-        answerEl.classList.add("btn-class")
-        presentNextQuestion()
-        
-    }
-    
-    
-        
-     /*  //TIMER for quiz  
-    function myTimer() {
-        document.querySelector('#timer').innerHTML = "Time: " + sec;
-        sec--;
-        if (sec == -1) {
-            clearInterval(time);
-            alert("Quiz Over");
-            //questionContainer.style.display = "none"
-            //insert last slide here closingContainer.style.remove ="hide"
-            //Display Score....
-        }   
-        var time = setInterval(myTimer, 1000);
-    }   */
