a6547a28a3fc54176eb3dd817300a0e1caa5b967
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2021-10-18 12:16:49 -0500

    final push before publish

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -20,7 +20,7 @@ let topScoreArr = []
 let currentQuestion, currentAnswer, answer, timerId, initialData, playerInitials, listScore
 let message, listPlayersScore, finalScores
 
-
+//Array of questions
 var questionArr = [
 
     {
@@ -72,10 +72,69 @@ var questionArr = [
             'Jarvis',
         ],
         answer: 'Rocket',
+    },
+    {
+        question: 'Who is the firstborn child of Odin?',
+        choices: [
+            'Loki',
+            'Hela',
+            'Steve Rodgers',
+            'Thor',
+        ],
+        answer: 'Hela',
+    },
+    {
+        question: 'Nick Fury wears a patch over which eye?',
+        choices: [
+            'Left eye',
+            'Right eye',
+            'Third eye',
+            'He wears an eye patch?',
+        ],
+        answer: 'Left eye',
+    },
+    {
+        question: 'Pepper Potts is allergic to what',
+        choices: [
+            'Peanuts',
+            'Gluten',
+            'Strawberries',
+            'Stark’s ego',
+        ],
+        answer: 'Strawberries',
+    },
+    {
+        question: 'How many infinity stones are in this universe?',
+        choices: [
+            'One',
+            'Four',
+            'Five',
+            'Six',
+        ],
+        answer: 'Six',
+    },
+    {
+        question: 'Who is the firstborn child of Odin?',
+        choices: [
+            'Loki',
+            'Hela',
+            'Steve Rodgers',
+            'Thor',
+        ],
+        answer: 'Hela',
+    },
+    {
+        question: 'Where is Captain America from?',
+        choices: [
+            'Brooklyn',
+            'Queens',
+            'Chicago',
+            'New Jersey',
+        ],
+        answer: 'Brooklyn',
     }
-    
-
 ]
+
 //Press Start button to begin game
 
 startButton.addEventListener("click", startQuiz)
@@ -87,6 +146,7 @@ answerChoices = questionArr[currentQuestion].choices
 question = questionArr[currentQuestion].question
 answer = questionArr[currentQuestion].answer
 
+//Begin quiz..Changes visual slides and starts timer
 function startQuiz() {
     myTimer()
     introContainer.style.display = "none"
@@ -95,11 +155,8 @@ function startQuiz() {
     timeId = setInterval(myTimer, 1000);
 }
 
-
-
-//Display current question in the button
+//Display current question in the button also conditions the timer
 function displayQuestion(question, answerChoices, answer) {
-    console.log('answerChoices: ', answerChoices);
     questionEl.innerText = question
     for (i = 0; i < answerChoices.length; i++) {
         var btn = document.querySelector(`#btn${i + 1}`)
@@ -110,6 +167,7 @@ function displayQuestion(question, answerChoices, answer) {
     if(sec === 0) () => endGame
 }
 
+//Validates the answer and changes time based on result
 function validateAnswer() {
     
     let selectedAnswer = this.dataset.text
@@ -132,15 +190,14 @@ function validateAnswer() {
     return
 }
 
-
-
+//clears the question and answer results
 function clearStatusClass() {
     currentQuestion++
     correctTitle.classList.add("hidden")
     wrongTitle.classList.add("hidden")
     answerEl.classList.add("btn-class")
     console.log(currentQuestion)
-    if (currentQuestion > 4 || sec === 0) {
+    if (currentQuestion > 9 || sec <= 0) {
         console.log(sec)
         console.log(currentQuestion.length);
         clearInterval(timeId)
@@ -152,61 +209,68 @@ function clearStatusClass() {
         displayQuestion(question, answerChoices, answer)
     }
 }
+
+//Timer for quiz
 function myTimer() {
     sec--;
-    timeShow.innerHTML = "Time: " + sec
-    
+    if(sec <= 0){
+        clearInterval(timerId)
+        endGame()}
+        timeShow.innerHTML = "Time: " + sec
+        
 }
-
+//End game slide and where initials are entered   
 function endGame(score) {
-    clearInterval(timeId)
-    questionContainer.style.display = "none"
-    endGamePage.style.display = "flex"
-    initialSumbit.addEventListener("click", saveInitials) 
+        clearInterval(timeId)
+        questionContainer.style.display = "none"
+        endGamePage.style.display = "flex"
+        initialSumbit.addEventListener("click", saveInitials) 
 }
 
+// Save Initials to local storage 
 function saveInitials (ev) {
-    ev.preventDefault() 
-    let initialData = {
-        initials: initialsText.value,
-        playerScore: score
-    }
-    if(localStorage.getItem('initialData') === null) {
-        localStorage.setItem('initialData', [] )
-    }
-    let scoreData = []
-    let oldScore = localStorage.getItem('initialData')
-    scoreData = oldScore ? JSON.parse(oldScore) : [] 
-    scoreData.push(initialData)
-    localStorage.setItem('initialData', JSON.stringify(scoreData))
-    console.log(scoreData)
-    showHighScores(initialData)
+        ev.preventDefault() 
+        let initialData = {
+            initials: initialsText.value,
+            playerScore: score
+        }
+        if(localStorage.getItem('initialData') === null) {
+            localStorage.setItem('initialData', [] )
+        }
+        let scoreData = []
+        let oldScore = localStorage.getItem('initialData')
+        scoreData = oldScore ? JSON.parse(oldScore) : [] 
+        scoreData.push(initialData)
+        localStorage.setItem('initialData', JSON.stringify(scoreData))
+        console.log(scoreData)
+        showHighScores(initialData)
 }
-
-
-function showHighScores(initialData) {
-    endGamePage.style.display = "none"
-    scoreSlide.style.display = "flex"
-    topScoreArr = []
-    let finalScores = localStorage.getItem('initialData')
-    topScoreArr = finalScores ? JSON.parse(finalScores) : []
-    console.log(topScoreArr)
-    //let playerInitials = topScoreArr[i].initials
-    console.log(topScoreArr)
-    //let listScore = topScoreArr[i].playerScore
-    topScoreArr.sort(function (a, b) {
-        return a.playerScore - b.playerScore
-    })
-    topScoreArr.reverse()
-    console.log(topScoreArr)
     
-    for(let i = 0; i <= 4 ; i++) {
-        let playerInitials = topScoreArr[i].initials
-        let listScore = topScoreArr[i].playerScore
-        let message = "Player Initials : " + playerInitials + " Players Score: " + listScore 
-        listPlayersScore = document.createElement("li")
-        listPlayersScore.innerHTML = message
-        topScoresList.appendChild(listPlayersScore)
-        console.log(listPlayersScore)
-    }
-} 
+//Take high scores from local storage and present them     
+function showHighScores(initialData) {
+        endGamePage.style.display = "none"
+        scoreSlide.style.display = "flex"
+        topScoreArr = []
+        let finalScores = localStorage.getItem('initialData')
+        topScoreArr = finalScores ? JSON.parse(finalScores) : []
+        console.log(topScoreArr)
+        //let playerInitials = topScoreArr[i].initials
+        console.log(topScoreArr)
+        //let listScore = topScoreArr[i].playerScore
+        topScoreArr.sort(function (a, b) {
+            return a.playerScore - b.playerScore
+        })
+        topScoreArr.reverse()
+        console.log(topScoreArr)
+        
+        for(let i = 0; i <= 4 ; i++) {
+            let playerInitials = topScoreArr[i].initials
+            let listScore = topScoreArr[i].playerScore
+            let message = "Player Initials :  " + playerInitials + " Players Score:  " + listScore 
+            listPlayersScore = document.createElement("li")
+            listPlayersScore.innerHTML = message
+            topScoresList.appendChild(listPlayersScore)
+            console.log(listPlayersScore)
+        }
+}
+ 
